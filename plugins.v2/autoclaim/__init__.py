d0db11c468dce8078127af833c9ec8fa5e4d4d75
黄渊 <2801788267@qq.com>
2025-08-09 15:16:21 +0800

    AutoClaim v1.0.1: claim seeds that meet the conditions based on seed size and seeding time.

--- plugins.v2/autoclaim/__init__.py
+++ plugins.v2/autoclaim/__init__.py
@@ -1,15 +1,17 @@
 import re
+import os
 import time
 import json
+import pytz
+
 from lxml import etree
 from urllib.parse import urljoin
 from datetime import datetime, timedelta
 from threading import Event
 from typing import Any, List, Dict, Tuple
-
-import pytz
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+from concurrent.futures import ThreadPoolExecutor, wait
 
 from app.core.config import settings
 from app.helper.cloudflare import under_challenge
@@ -18,6 +20,7 @@ from app.db.site_oper import SiteOper
 from app.log import logger
 from app.plugins import _PluginBase
 from app.utils.http import RequestUtils
+from app.utils.string import StringUtils
 from app.schemas import NotificationType
 
 
@@ -29,7 +32,7 @@ class AutoClaim(_PluginBase):
     # 插件图标
     plugin_icon = "seed.png"
     # 插件版本
-    plugin_version = "1.0.0"
+    plugin_version = "1.0.1"
     # 插件作者
     plugin_author = "hyuan280"
     # 作者主页
@@ -43,6 +46,8 @@ class AutoClaim(_PluginBase):
 
     # 私有属性
     _scheduler = None
+    _thread_pool = None
+    _futures = []
 
     # 开关
     _enabled = False
@@ -50,6 +55,9 @@ class AutoClaim(_PluginBase):
     _onlyonce = False
     _sites = None
     _notify = False
+    _queues = 1
+    _size = 0
+    _seedtime = 0
     # 退出事件
     _event = Event()
     _site_infos = []
@@ -66,6 +74,16 @@ class AutoClaim(_PluginBase):
             self._cron = config.get("cron")
             self._notify = config.get("notify")
             self._sites = config.get("sites", [])
+            self._queues = config.get("queues", 1)
+            self._size = config.get("size", 0)
+            self._seedtime = config.get("seedtime", 0)
+
+        if isinstance(self._queues, str):
+            self._queues = int(self._queues)
+        if isinstance(self._size, str):
+            self._size = int(self._size)
+        if isinstance(self._seedtime, str):
+            self._seedtime = int(self._seedtime)
 
         # 停止现有任务
         self.stop_service()
@@ -99,6 +117,8 @@ class AutoClaim(_PluginBase):
                 self.update_config(config=config)
                 return
 
+            # 线程池
+            self._thread_pool = ThreadPoolExecutor(max_workers=self._queues)
             # 定时服务
             self._scheduler = BackgroundScheduler(timezone=settings.TZ)
 
@@ -154,6 +174,7 @@ class AutoClaim(_PluginBase):
         """
         拼装插件配置页面，需要返回两块数据：1、页面配置；2、数据结构
         """
+        cpu_options = [{"title": n, "value": n} for n in range(1, os.cpu_count() + 1)]
         site_options = []
         for site in SitesHelper().get_indexers():
             if site.get("schema", "").startswith("Nexus"):
@@ -242,6 +263,63 @@ class AutoClaim(_PluginBase):
                             {
                                 'component': 'VCol',
                                 'props': {
+                                    'cols': 12,
+                                    'md': 4,
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VSelect',
+                                        'props': {
+
+                                            'model': 'queues',
+                                            'label': '同时处理站点数目',
+                                            'items': cpu_options
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 4,
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VTextField',
+                                        'props': {
+                                            'model': 'size',
+                                            'label': '认领的最小种子大小(MB)',
+                                            'placeholder': '0 不限制'
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                    'md': 4,
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VTextField',
+                                        'props': {
+                                            'model': 'seedtime',
+                                            'label': '认领的最小做种时间(小时)',
+                                            'placeholder': '0 不限制'
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
                                     'cols': 12
                                 },
                                 'content': [
@@ -268,6 +346,9 @@ class AutoClaim(_PluginBase):
             "onlyonce": False,
             "cron": "",
             "sites": "",
+            "queues": 1,
+            "size": 0,
+            "seedtime": 0,
         }
 
     def get_page(self) -> List[dict]:
@@ -343,6 +424,18 @@ class AutoClaim(_PluginBase):
             logger.error(f"解析网页错误：{e}")
             return None, ""
 
+    def _task_auto_claim(self, site, userid):
+        page, html_text = self._get_user_torrent_seeding_html(site, userid)
+        self._addClaim(site, page, html_text)
+
+        while page:
+            if self._event.is_set():
+                logger.info(f"认领种子服务停止")
+                return
+
+            page, html_text = self._get_user_torrent_seeding_html(site, userid, page)
+            self._addClaim(site, page, html_text)
+
     def auto_claim(self):
         """
         开始种子保活
@@ -352,6 +445,7 @@ class AutoClaim(_PluginBase):
         if not self.__validate_config():
             return
 
+        self._futures = []
         self._claim_result = {}
         for site in self._site_infos:
             if self._event.is_set():
@@ -363,18 +457,24 @@ class AutoClaim(_PluginBase):
                 continue
             else:
                 userdata = userdatas[-1]
-            logger.info(f"userid={userdata.userid}")
-            page, html_text = self._get_user_torrent_seeding_html(site, userdata.userid)
-            self._addClaim(site, html_text)
+            logger.info(f"站点 {site.name} 用户id {userdata.userid}")
 
-            while page:
-                page, html_text = self._get_user_torrent_seeding_html(site, userdata.userid, page)
-                self._addClaim(site, html_text)
+            # 提交任务到线程池
+            future = self._thread_pool.submit(self._task_auto_claim, site, userdata.userid)
+            if future:
+                self._futures.append(future)
+            else:
+                logger.error(f"任务提交到线程失败，站点：{site.name}")
+                continue
+
+        if self._futures:
+            wait(self._futures)
 
         message_text = ""
         for k, v in self._claim_result.items():
             message_text += f"站点 {k}：\n"
             message_text += f" 认领    成功：{v.get('success')}\n"
+            message_text += f" 认领    跳过：{v.get('skip')}\n"
             message_text += f" 认领人数上限：{v.get('excessive')}\n"
             message_text += f" 认领    失败：{v.get('failed')}\n\n"
 
@@ -387,15 +487,74 @@ class AutoClaim(_PluginBase):
                 text=message_text
                 )
 
-    def _addClaim(self, site, html_text):
+    @staticmethod
+    def _parse_torrent_info(html):
+        torrent_info = {}
+        def time_span_to_seconds(time_str):
+            # 匹配天数、小时、分钟、秒
+            pattern = r"(?:(\d+)\s*[天日dD])?\s*(?:(\d+):)?(\d+):(\d+)"
+            match = re.match(pattern, time_str)
+            if not match:
+                raise ValueError(f"无法解析时间格式: {time_str}")
+
+            days = int(match.group(1) or 0)
+            hours = int(match.group(2) or 0)
+            minutes = int(match.group(3))
+            seconds = int(match.group(4))
+
+            total_seconds = (days * 24 * 3600) + (hours * 3600) + (minutes * 60) + seconds
+            return total_seconds
+        time_index = 0
+        size_index = 0
+
+        headers = html.xpath("//tr[1]/td")
+        for index, header in enumerate(headers, start=1):
+            text = header.xpath("string()").strip()
+            if "做种时间" in text or "Time" in text:
+                time_index = index
+                continue
+            if header.xpath(".//img[contains(@class,'size')]"):
+                size_index = index
+                continue
+
+        if time_index == 0 or size_index == 0:
+            return torrent_info
+
+        trs = html.xpath("//tr")
+        for tr in trs[1:]:
+            torrent_id = 0
+            torrent_links = tr.xpath(".//a[contains(@href, 'id=')]/@href")
+            if torrent_links:
+                match = re.search(r'id=(\d+)', torrent_links[0])
+                if match:
+                    torrent_id = (int(match.group(1)))
+            if torrent_id == 0:
+                continue
+            size_text = tr.xpath(f"./td[{size_index}]/text()")
+            time_text = tr.xpath(f"./td[{time_index}]/text()")
+            if size_text and time_text:
+                try:
+                    size = StringUtils.num_filesize(''.join(size_text).strip()) # 字节
+                    time = time_span_to_seconds(''.join(time_text).strip()) # 秒
+                except Exception as e:
+                    logger.error(f"解析种子信息错误：{e}")
+                    continue
+
+                torrent_info[torrent_id] = { "time": int(time/(60*60)), "size": int(size/(1024*1024)) }
+
+        return torrent_info
+
+    def _addClaim(self, site, page, html_text):
         cur_err_cnt = 0 # 解析错误
         success_cnt = 0 # 成功认领
+        skip_cnt = 0 # 条件不满足跳过认领
         excessive_cnt = 0 # 认领人数过多
         failed_cnt = 0 #认领失败
 
         if self._claim_result.get(site.name):
             old_result = self._claim_result.get(site.name)
             success_cnt = old_result.get("success")
+            skip_cnt = old_result.get("skip")
             excessive_cnt = old_result.get("excessive")
             failed_cnt = old_result.get("failed")
 
@@ -406,9 +565,14 @@ class AutoClaim(_PluginBase):
             "User-Agent": f"{site.ua}"
         }
         html = etree.HTML(html_text)
-        logger.debug(f"html={html}")
+        try:
+            torrent_infos = self._parse_torrent_info(html)
+        except Exception as e:
+            logger.error(f"解析种子信息页面错误：{e}")
+            return
+
         elements = html.xpath("//button[@data-action='addClaim' and not(contains(@style, 'display: none'))]")
-        logger.debug(f"页面有种子 {len(elements)} 个可以认领")
+        logger.info(f"站点 {site.name} 页面 {page} 有种子 {len(elements)} 个可以认领")
         for e in elements:
             if self._event.is_set():
                 logger.info(f"认领种子服务停止")
@@ -418,7 +582,19 @@ class AutoClaim(_PluginBase):
 
             torrent_id = e.get('data-torrent_id')
             if not torrent_id:
+                failed_cnt += 1
                 continue
+
+            torrent_info = torrent_infos.get(torrent_id)
+            if torrent_info:
+                if self._seedtime and torrent_info.get("time") < self._seedtime:
+                    logger.debug(f"站点 {site.name} 种子 {torrent_id}: 做种时间不足（{torrent_info.get("time")}）小时")
+                    skip_cnt += 1
+                    continue
+                if self._size and torrent_info.get("size") < self._size:
+                    logger.debug(f"站点 {site.name} 种子 {torrent_id}: 大小不够（{torrent_info.get("size")}）MB")
+                    skip_cnt += 1
+                    continue
 
             data = {
                 "action": "addClaim",
@@ -426,7 +602,7 @@ class AutoClaim(_PluginBase):
             }
 
             time.sleep(0.5)
-            logger.debug(f"认领站点 {site.name} 种子 {torrent_id}")
+            logger.info(f"认领站点 {site.name} 种子 {torrent_id}")
             res = RequestUtils(cookies=site.cookie,
                                 timeout=60,
                                 proxies=site.proxy,
@@ -451,6 +627,7 @@ class AutoClaim(_PluginBase):
 
         self._claim_result[site.name] = {
             "success": success_cnt,
+            "skip": skip_cnt,
             "excessive": excessive_cnt,
             "failed": failed_cnt,
         }
@@ -466,13 +643,19 @@ class AutoClaim(_PluginBase):
         """
         退出插件
         """
+        self._event.set()
+        if self._thread_pool:
+            try:
+                self._thread_pool.shutdown(wait=True)
+            except Exception as e:
+                logger.error(f"线程池关闭失败：{e}")
+            self._thread_pool = None
         try:
             if self._scheduler:
                 self._scheduler.remove_all_jobs()
                 if self._scheduler.running:
-                    self._event.set()
                     self._scheduler.shutdown()
-                    self._event.clear()
                 self._scheduler = None
         except Exception as e:
-            print(str(e))
+            logger.error(f"停止调度器失败：{e}")
+        self._event.clear()
